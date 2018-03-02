b9b9fcc645697b9909cb8098fefffb18a083193b
Daniel Harasim <daniel.harasim@gmx.de>
2018-03-02 18:06:06 +0100

    add code and tests

--- src/AccessMacro.jl
+++ src/AccessMacro.jl
@@ -1,5 +1,26 @@
 module AccessMacro
 
-# package code goes here
+using MacroTools
+export @access
+
+get_fieldname(expr::Expr)   = expr.args[1]
+get_fieldname(symb::Symbol) = symb
+
+function field_to_function_expr(f, T)
+    s = gensym()
+    :(function $f($s :: $T)
+        $s.$f
+    end)
+    #:( ($f)($s :: $T) = $s.$f )
+end
+
+macro access(expr)
+    return_expr = Expr(:block)
+    @capture(expr, struct (T_{P__}|T_) fields__ end | type (T_{P__}|T_) fields__ end | mutable struct (T_{P__}|T_) fields__ end | immutable (T_{P__}|T_) fields__ end)
+    push!(return_expr.args, expr)
+    push!(return_expr.args, field_to_function_expr.(get_fieldname.(fields), T)...)
+    push!(return_expr.args, T)
+    esc(return_expr)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,32 @@
 using AccessMacro
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@access struct A
+    x :: Int
+end
+
+@access mutable struct B{T}
+    x
+    y :: T
+end
+
+@access type C{S, T}
+    x :: S
+    y :: T
+end
+
+@access immutable D
+    x
+end
+
+@test x(A(3)) == 3
+
+@test x(B(1, "2")) == 1 && y(B(1, "2")) == "2"
+
+@test x(C(1.0, [2])) == 1.0 && y(B(1.0, [2])) == [2]
+
+@test x(D(43)) == 43
+
+using PkgDev
+
+PkgDev.generate("AccessMacro", "MIT")
